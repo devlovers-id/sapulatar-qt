fa3f6bb0ea593608a4a396fa1fc2417a7bd9b38a
Sofyan Sugianto <sofyanartem@gmail.com>
2021-09-09 13:53:01 +0700

    update entrypoint

--- main.py
+++ main.py
@@ -3,26 +3,13 @@ import sys
 import os
 
 
-from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtWidgets import QApplication, QWidget, QMainWindow
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
-
-class bgcleaner(QWidget):
-    def __init__(self):
-        super(bgcleaner, self).__init__()
-        self.load_ui()
-
-    def load_ui(self):
-        loader = QUiLoader()
-        path = os.path.join(os.path.dirname(__file__), "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
-        ui_file.close()
-
 if __name__ == "__main__":
-    app = QApplication([])
-    widget = bgcleaner()
-    widget.show()
-    sys.exit(app.exec_())
+    loader = QUiLoader()
+    app = QApplication(sys.argv)
+    main_window = loader.load("form.ui", None)
+    main_window.show()
+    app.exec_()
