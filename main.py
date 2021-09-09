9706001eded1ad1f71f882510fa0b9de7fc7d469
Rania Amina <reaamina@gmail.com>
2021-09-09 11:15:24 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+
+
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+
+class bgcleaner(QWidget):
+    def __init__(self):
+        super(bgcleaner, self).__init__()
+        self.load_ui()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = bgcleaner()
+    widget.show()
+    sys.exit(app.exec_())
