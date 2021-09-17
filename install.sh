83095312000dc18ef9d35fe8581225c2155f6456
Rania Amina <reaamina@gmail.com>
2021-09-17 14:46:05 +0700

    update install script

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 mainDir=$HOME/.local/share/devlovers-id
 
-# if [[ $1 == "--uninstall" ]]; then
-#     echo "Uninstalling"
-#     rm -rf $mainDir
-#     rm $HOME/.local/share/icons/sapulatar-qt.png
-#     rm $HOME/.local/bin/sapulatar-qt
-#     rm $HOME/.local/share/applications/sapulatar-qt.desktop
-#     exit
-# fi
+if [[ $1 == "--uninstall" ]]; then
+    echo "Uninstalling"
+    rm -rf $mainDir
+    rm $HOME/.local/share/icons/sapulatar-qt.png
+    rm $HOME/.local/bin/sapulatar-qt
+    rm $HOME/.local/share/applications/sapulatar-qt.desktop
+    exit
+fi
 
 # Checking dependencies
 echo -e "Checking dependencies:"
@@ -65,7 +65,7 @@ cp $mainDir/sapulatar-qt/assets/sapulatar-qt.png $HOME/.local/share/icons/sapula
 
 echo "Copying executable"
 echo -e "#!/bin/bash\n" >> /$HOME/.local/bin/sapulatar-qt
-echo -e "if [[ $1 == \"--uninstall\" ]]; then\n    echo \"Uninstalling\"\n    rm -rf $mainDir\n    rm $HOME/.local/share/icons/sapulatar-qt.png\n    rm $HOME/.local/bin/sapulatar-qt\n    rm $HOME/.local/share/applications/sapulatar-qt.desktop\n    exit\nfi >> $HOME/.local/bin/sapulatar-qt
+echo -e "if [[ \$1 == \"--uninstall\" ]]; then\n    echo \"Uninstalling\"\n\trm -rf $mainDir\n\trm $HOME/.local/share/icons/sapulatar-qt.png\n\trm $HOME/.local/bin/sapulatar-qt\n\trm $HOME/.local/share/applications/sapulatar-qt.desktop\n\texit\nfi" >> $HOME/.local/bin/sapulatar-qt
 echo "cd $(echo $mainDir/sapulatar-qt)" >> $HOME/.local/bin/sapulatar-qt
 echo -e "\npython3 main.py" >> $HOME/.local/bin/sapulatar-qt
 chmod +x $HOME/.local/bin/sapulatar-qt
