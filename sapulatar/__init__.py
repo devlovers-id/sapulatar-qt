c55cfb40651bcac287698bb472428b19d4655b37
Sofyan Sugianto <sofyanartem@gmail.com>
2021-09-22 16:42:12 +0700

    update web url

--- sapulatar/__init__.py
+++ sapulatar/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "1.0.0"
-__website_url__ = "https://dev-is.my.id"
+__website_url__ = "https://github.com/devlovers-id/sapulatar-qt"
 __author__ = "Devlovers ID"
 __title__ = "Sapulatar-Qt"
